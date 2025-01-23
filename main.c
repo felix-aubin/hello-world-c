d28026982b979579aa08eb17c01c8aa449f5b16d
felix-aubin <e6316064@cmaisonneuve.qc.ca>
2025-01-23 15:36:15 +0000

    Ajout activité 2

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include "math_operations.h"
+#include "math_operations.c"
 
 int main() {
+    int a = 10, b = 7;
+    
+
     printf("Hello, World!\n");
+
+    printf("%d + %d = %d\n", a, b, addition(a, b));
+    printf("%d - %d = %d\n", a, b, soustraction(a, b));
     return 0;
 }
